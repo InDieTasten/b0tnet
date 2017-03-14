ebc2e8c28c97993a186c457bf2700b8958805893
In Die Tasten <indietasten@gmail.com>
2017-03-14 20:18:45 +0100

    setups (basics work)

---
+++ app/index.ts
@@ -0,0 +1,10 @@
+
+import { Terminal } from "./terminal"
+
+let thing = new Terminal();
+
+thing.test = () => {
+    console.info("Isn't this interesting :)");
+};
+
+thing.test();
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,28 @@
+var path = require("path")
+
 module.exports = {
- entry: './index.ts',
+ entry: './app/index.ts',
  output: {
    filename: 'bundle.js',
-   path: __dirname
+   path: path.resolve(__dirname, "build"),
+   publicPath: "/assets/"
  },
  module: {
    rules: [
      {
-       test: /\.ts$/,
-       loader: 'ts-loader',
-       exclude: /node_modules/,
+       enforce: 'pre',
+       test: /\.js$/,
+       loader: "source-map-loader"
      },
+     {
+       enforce: 'pre',
+       test: /\.ts$/,
+       use: "source-map-loader"
+     }
    ]
  },
  resolve: {
    extensions: [".ts", ".js"]
  },
- devtool: 'inline-source-map'
+ devtool: 'inline-source-map',
 };
