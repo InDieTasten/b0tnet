34357779dad26c1ed8926a5d75a5c4e34e921cf4
In Die Tasten <indietasten@gmail.com>
2017-03-26 23:07:28 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
-/// <refer ence path="../node_modules/strongly-typed-events/strongly-typed-events.d.ts" />
-
-import * as _ from 'strongly-typed-events'
-
 import { Terminal } from "./terminal"
 import { color_names } from "./utility/color16"
 
-declare require;
-let x = require("strongly-typed-events");
+let dslify = require("dslify");
 
 console.info("B0tnet Game launching...");
 
@@ -14,14 +9,12 @@ console.info("B0tnet Game launching...");
     // initialize game
     document.body.style.backgroundColor = "#101010";
 
-    EventDispatcher.apply
-
     // create dom element for terminal
     let nativeTerminal = document.createElement("div");
     document.body.appendChild(nativeTerminal);
 
     // attach terminal to dom element
-    let terminalObj = new Terminal(nativeTerminal, { width: 81, height: 25 });
+    let terminalObj = new Terminal(nativeTerminal, { width: 101, height: 45 });
 
     terminalObj.display();
     setInterval(() => {
@@ -32,23 +25,23 @@ console.info("B0tnet Game launching...");
             text += possible.charAt(Math.floor(Math.random() * possible.length));
 
         terminalObj.setCursorPos({
-            x: 0.5 + Math.random() * 73,
-            y: 0.5 + Math.random() * 24
+            x: 0.5 + Math.random() * 93,
+            y: 44
         });
         terminalObj.setTextColor(Math.ceil(Math.random()*16)-1);
         terminalObj.write("0x" + text + " ");
 
-    }, 10);
+    }, 20);
 
     setInterval(() => {
         terminalObj.scroll(1);
-    }, 30);
+    }, 20);
 
     (function draw() {
         setTimeout(() => {
             requestAnimationFrame(draw);
             terminalObj.display();
-        }, 1000/30);
+        }, 1000/60);
     })();
     
 
