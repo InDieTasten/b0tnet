88e0aa1aa705ae08f2e2ac153e9c077a5ab439c3
InDieTasten <indietasten@gmail.com>
2019-11-01 20:30:53 +0100

    Launch vm after window.onload

--- src/index.ts
+++ src/index.ts
@@ -7,28 +7,32 @@ import { Shell } from './programs/shell';
 import { ALongTimeAgo } from './programs/alongtimeago';
 
 (function Startup() {
-    // create dom element for terminal
-    let terminalElement = document.createElement("div");
-    document.body.appendChild(terminalElement);
 
-    
-    let nativeTerm = new Terminal({
-        scrollback: 0,
-        fontSize: 22,
-        cursorBlink: false,
-        rendererType: "canvas",
-        allowTransparency: true,
-        theme: {
-            background: "rgba(0,0,0,0)"
-        }
+    window.addEventListener("load", () => {
+        // create dom element for terminal
+        let terminalElement = document.createElement("div");
+        document.body.appendChild(terminalElement);
+
+        let nativeTerm = new Terminal({
+            scrollback: 0,
+            fontSize: 22,
+            cursorBlink: false,
+            rendererType: "canvas",
+            allowTransparency: true,
+            theme: {
+                background: "rgba(0,0,0,0)"
+            }
+        });
+
+        let virtualMachine = new Machine([
+            Shell.Program,
+            ALongTimeAgo.Program
+        ]);
+
+        nativeTerm.loadAddon(virtualMachine);
+        nativeTerm.open(terminalElement);
+        nativeTerm.resize(80, 25);
     });
 
-    let virtualMachine = new Machine([
-        Shell.Program,
-        ALongTimeAgo.Program
-    ]);
-
-    nativeTerm.loadAddon(virtualMachine);
-    nativeTerm.open(terminalElement);
-    nativeTerm.resize(80, 25);
+    
 }());
