8c12200962d2f6255f35959a799a733d3fddbec7
InDieTasten <indietasten@gmail.com>
2019-11-01 20:01:55 +0100

    Switch to canvas rendering

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,6 @@ import { Machine } from './machine';
 import { Shell } from './programs/shell';
 import { ALongTimeAgo } from './programs/alongtimeago';
 
-let vm = new VM();
-
-console.log("B0tnet Game launching...");
-
 (function Startup() {
     // create dom element for terminal
     let terminalElement = document.createElement("div");
@@ -20,12 +16,13 @@ console.log("B0tnet Game launching...");
         scrollback: 0,
         fontSize: 22,
         cursorBlink: false,
-        rendererType: "dom",
+        rendererType: "canvas",
         allowTransparency: true,
         theme: {
             background: "rgba(0,0,0,0)"
         }
     });
+
     let virtualMachine = new Machine([
         Shell.Program,
         ALongTimeAgo.Program
@@ -34,33 +31,4 @@ console.log("B0tnet Game launching...");
     nativeTerm.loadAddon(virtualMachine);
     nativeTerm.open(terminalElement);
     nativeTerm.resize(80, 25);
-    // nativeTerm.write('BrowserOS 0.1');
-    // nativeTerm.write('\r\n$ ');
-    // nativeTerm.onLineFeed(() => {
-    //     console.log("onLineFeed");
-    // });
-    // nativeTerm.onCursorMove(() => {
-    //     console.log("onCursorMove");
-    // });
-    // nativeTerm.onData(e => {
-    //     console.log("onData: ", e);
-    // })
-    // nativeTerm.onKey(e => {
-    //     console.log("onKey: ", e);
-
-    //     const printable = !e.domEvent.altKey && !e.domEvent.ctrlKey && !e.domEvent.metaKey;
-
-    //     if (e.domEvent.key == "Enter") {
-    //         nativeTerm.write('\r\n$ ');
-    //     } else if (e.domEvent.key === 'Backspace') {
-    //         Do not delete the prompt
-    //         if (nativeTerm.buffer.cursorX > 2) {
-    //             nativeTerm.write('\b \b');
-    //         }
-    //     } else if (printable) {
-    //         nativeTerm.write(e.key);
-    //     }
-    // });
 }());
-
-console.log("B0tnet Game launched successfully :)");
