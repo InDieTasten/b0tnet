33d1f8e069feb11f20dbf79ee850fe35be6dd244
InDieTasten <indietasten@gmail.com>
2019-11-27 01:32:01 +0100

    Add multiple spheres and materials

--- src/programs/raytrace.ts
+++ src/programs/raytrace.ts
@@ -1,5 +1,6 @@
 import { Process } from "../os/process";
 import { TimeoutEvent } from "../os/apis/os";
+import { ansiBackgroundColor, rgbTo256, ansiReset } from "./helpers/color256";
 
 class Vector3f {
     public constructor(x?: number, y?: number, z?: number) {
@@ -29,97 +30,153 @@ class Vector3f {
         const product = this.multiply(other);
         return product.x + product.y + product.z;
     }
+    public magnitude(): number {
+        return Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
+    }
 }
 
 interface RenderObject {
-    intersects(position: Vector3f, direction: Vector3f): boolean;
+    material: Material;
+    findIntersect(position: Vector3f, direction: Vector3f): Vector3f;
+    getNormal(closestPoint: Vector3f): Vector3f;
+}
+
+class Material {
+    constructor(
+        public diffuseColor: Vector3f
+    ) {}
 }
 
 class Sphere implements RenderObject {
 
     center: Vector3f;
     radius: number;
+    material: Material;
 
-    constructor(position?: Vector3f, radius?: number) {
+    constructor(position?: Vector3f, radius?: number, material?: Material) {
         this.center = position || new Vector3f();
         this.radius = radius || 1;
+        this.material = material || new Material(new Vector3f(0.2, 0.2, 0.2));
     }
 
-    public intersects(position: Vector3f, direction: Vector3f): boolean {
+    public findIntersect(position: Vector3f, direction: Vector3f): Vector3f|null {
         // ray to sphere intersection algorithm
         const oc = position.subtract(this.center);
         const a = direction.dot(direction);
         const b = 2 * oc.dot(direction);
         const c = oc.dot(oc) - this.radius * this.radius;
         const discriminant = b*b - 4*a*c;
-
-        if (discriminant < 0) return false;
+        if (discriminant < 0) return null;
         const distance = (-b - Math.sqrt(discriminant)) / (2*a);
 
-        return distance > 0;
+        // this prevents finding intersections opposite of the ray direction
+        if (distance <= 0) return null;
+
+        // calculate the point of intersection
+        return position.add(direction.multiply(new Vector3f(distance, distance, distance)));
     }
+
+    public getNormal(point: Vector3f): Vector3f {
+        return point.subtract(this.center).normalize();
+    }
+}
+
+class PointLight {
+    constructor(
+        public position: Vector3f,
+        public intensity: number
+    ) {}
+}
+
+class Scene {
+    public constructor(
+        public objects: Sphere[],
+        public lights: PointLight[]) {}
 }
 
 export class RaytraceProgram extends Process {
 
-    private sphere: Sphere = new Sphere(new Vector3f(0,0,-16), 2);
-
     private fov: number = Math.PI/2; // 90 degrees
     private width: number;
     private height: number;
-    private framebuffer: Float32Array;
+    private framebuffer: string[];
 
     async main(args: string[]): Promise<number> {
 
         const terminalSize = await this.term.getSize();
         this.width = terminalSize.width;
         this.height = terminalSize.height;
-        this.framebuffer = new Float32Array(this.width * this.height);
+        this.framebuffer = [];
+
+        let scene = new Scene([
+            new Sphere(new Vector3f( 4,0,-5), 3, new Material(new Vector3f(1,0,0))),
+            new Sphere(new Vector3f(-4,0,-5), 3, new Material(new Vector3f(0,0,1)))
+        ], [
+            new PointLight(new Vector3f(-3, 4, 0), 5)
+        ]);
         
         let timerId = this.os.startTimer(0);
         while (true) {
             const event = await this.os.pollEvent();
             if (event instanceof TimeoutEvent && event.timerId === timerId) {
-                timerId = this.os.startTimer(10);
-                this.sphere.center = this.sphere.center.multiply(new Vector3f(1, 1, 0.995));
-                this.render(this.sphere);
+                timerId = this.os.startTimer(50);
+                scene.objects.forEach(object => object.center = object.center.add(new Vector3f(0, 0, 0.01)));
+                scene.lights.forEach(light => light.position = light.position.add(new Vector3f(0, 0, 0.01)));
+                this.render(scene);
                 this.display();
             }
         }
     }
 
     private async display(): Promise<void> {
-        let outputBuffer = '\n\n';
-        for (let i = 0; i < this.framebuffer.length; i++) {
-            if (this.framebuffer[i] === 0) {
-                outputBuffer += '.';
-            } else {
-                outputBuffer += '+';
-            }
-        }
-
-        await this.io.stdout.write(outputBuffer);
+        await this.io.stdout.write(ansiReset() + "\n\n" + this.framebuffer.join(''));
     }
 
-    private render(sphere: Sphere) {
+    private render(scene: Scene) {
         const origin = new Vector3f();
         for (let j = 0; j < this.height; j++) {
             for (let i = 0; i < this.width; i++) {
                 const x = (2* (i+0.5) / this.width-1) * Math.tan(this.fov/2) * this.width / this.height;
-                const y = (2 * (j+0.5) / this.height-1) * Math.tan(this.fov/1.45);
+                const y = (2 * (j+0.5) / this.height-1) * Math.tan(this.fov/2); // essentially changing the vertical fov to correct for the pixel height
                 const direction = new Vector3f(x, y, -1).normalize();
 
-                this.framebuffer[i + j * this.width] = this.castRay(origin, direction, sphere);
+                this.framebuffer[i + j * this.width] = this.castRay(origin, direction, scene);
             }
         }
     }
 
-    private castRay(position: Vector3f, direction: Vector3f, object: RenderObject): number {
-        let distance;
-        if (!object.intersects(position, direction)) {
-            return 0;
+    private castRay(position: Vector3f, direction: Vector3f, scene: Scene): string {
+        
+        let closestDistance = Infinity;
+        let closestPoint: Vector3f = null;
+        let closestObject: RenderObject = null;
+
+        scene.objects.forEach(object => {
+            let intersection = object.findIntersect(position, direction);
+            if (intersection) {
+                let distance = intersection.magnitude();
+                if (distance < closestDistance) {
+                    closestDistance = distance;
+                    closestPoint = intersection;
+                    closestObject = object;
+                }
+            }
+        });
+
+        if (closestObject) {
+
+            let normal = closestObject.getNormal(closestPoint);
+            let diffuseLightIntensity = 0;
+            scene.lights.forEach(light => {
+                let lightDirection = light.position.subtract(closestPoint).normalize();
+                diffuseLightIntensity += light.intensity * Math.max(0, lightDirection.dot(normal));
+            });
+
+            let rgb = closestObject.material.diffuseColor.multiply(new Vector3f(diffuseLightIntensity, diffuseLightIntensity, diffuseLightIntensity)).normalize();
+            return ansiBackgroundColor(rgbTo256(rgb.x, rgb.y, rgb.z)) + ' ';
         } else {
-            return 1;
+            // ambient character
+            return ansiReset() + ' ';
         }
     }
 }
